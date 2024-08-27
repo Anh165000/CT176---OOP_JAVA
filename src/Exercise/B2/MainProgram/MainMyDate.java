6d8d95367a907d710e196d2670b4b493e7ddd0b6
Anh165000 <phamduyanh.01062005da@gmail.com>
2024-08-27 22:10:22 +0700

    fixed

--- src/Exercise/B2/MainProgram/MainMyDate.java
+++ src/Exercise/B2/MainProgram/MainMyDate.java
@@ -1,13 +1,31 @@
 package Exercise.B2.MainProgram;
 
+import java.util.Scanner;
 import Exercise.B2.Object.MyDate;
+
 public class MainMyDate {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static int n;
+
     public static void main(String[] args) {
         MyDate today = new MyDate();
         today.inputDate();
-        // today.printDate();
-        // today.nextDate();
-        today.nextNDate(546);
+        getN();
+        today.nextNDate(n);
         today.printDate();
+    }
+
+    private static void getN() {
+        while(true){
+            try {
+                System.out.print("enter the next number of days: ");
+                n = Integer.parseInt(scanner.nextLine());
+                if(n > 0)
+                    break;
+            } catch (Exception e) {
+                System.out.println("enter the number and bigger than zero");
+            }
+        }
     }
 }
